c5f8822ded7213aa4b457f46ea28520d20d8d693
goodcantor <57045864+goodcantor@users.noreply.github.com>
2021-11-11 18:25:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,4 +21,21 @@ window.onscroll = function () {
     navbar.classList.remove('header-background');
   }
 };
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(86405112, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86405112" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 //# sourceMappingURL=main.js.map
